aba63e93818f93c4f19ec57c8718dab1fe231a2b
Bilash <bilashcse@gmail.com>
2017-02-16 18:55:20 +0600

    init

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var ElectronPDF = require('electron-pdf');
 var express = require('express');
-var bodyParser = require('body-parser');
 var promise = require('bluebird');
 var ejs = require('ejs');
 var fs = require('fs');
@@ -12,65 +11,42 @@ var app = express();
 var exporter = new ElectronPDF();
 
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.get('/download',  function (req, res) {
-
-	this.htmlUrl =  __dirname+'/email.html';
-	this.tempUrl = __dirname+'/tmp/invoice_'+invoiceId+'.html';
-
+	var input = __dirname+'/index.html';
+	var output = __dirname+'/output.pdf';
 	return new promise(function(resolve,reject){
-		
-		return shopInfo(shopId,token).bind({})
-		.then(function(shop){
-			this.shop = shop;
-			return getInvoiceData(shopId, invoiceId, token);
-		}).then(function(invoice){
-			return gatherInvoiceData(invoice, this.shop);
-		}).then(function(result){
-
-			return generateDynamicHtml(result, this.htmlUrl, this.tempUrl);
-		})
-		.then(function(url){
-			return generateInvoicePdf(url, invoiceId);
-		}).then(function(pdfUrl){
-			this.pdfUrl = pdfUrl;
-			return download(pdfUrl, invoiceId, res);
-		}).then(function(){
-			fs.unlinkSync(this.pdfUrl);
-			fs.unlinkSync(this.tempUrl);
+		return generatePDF(input, output)
+		.then(function(pdfUrl){
+			console.log("PDF conversion Done. URL : ", pdfUrl);
 			return resolve(true);
 		}).catch(function(err){
-			console.log("Failed",err.message);
-			res.end();
+			reject(err);
 		});
 	});	
 });
 
 
 
-function generateDynamicHtml(invoice, htmlUrl, output){
+// function generateDynamicHtml(invoice, htmlUrl, output){
 	
-	return new promise(function(resolve, reject){
-		console.log(invoice)
-		return ejs.renderFile(htmlUrl, { invoice : invoice },function (err, html) {
-			if(err)
-				reject(new Error(err.message));
-			else if(html){
-				fs.writeFile(output,html, function (err) {
-					if(err)
-						reject(new Error(err.message));
-					else
-						resolve(output);
-				});
-			}
-		});
-	});
-}
+// 	return new promise(function(resolve, reject){
+// 		console.log(invoice)
+// 		return ejs.renderFile(htmlUrl, { invoice : invoice },function (err, html) {
+// 			if(err)
+// 				reject(new Error(err.message));
+// 			else if(html){
+// 				fs.writeFile(output,html, function (err) {
+// 					if(err)
+// 						reject(new Error(err.message));
+// 					else
+// 						resolve(output);
+// 				});
+// 			}
+// 		});
+// 	});
+// }
 
-function generateInvoicePdf(pdfUrl, invoiceId){
+function generatePDF(input, output){
 	var jobOptions = {
 		inMemory: false,
 		closeWindow: false
@@ -82,9 +58,7 @@ function generateInvoicePdf(pdfUrl, invoiceId){
 		marginsType: 0
 	};
 
-	var output = 'pdf/invoice-'+invoiceId+'.pdf';
-
-	return exporter.createJob(pdfUrl, output, options, jobOptions)
+	return exporter.createJob(input, output, options, jobOptions)
 	.then(function (job) {
 		job.render();
 		return new promise(function(resolve){
